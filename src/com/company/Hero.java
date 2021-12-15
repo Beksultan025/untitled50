5a29047a1d692c30e98b05c080e4bd4aacbaad40
Beksultan025 <ukuevb08@gmail.com>
2021-12-15 23:11:13 +0600

    Initial commit

---
+++ src/com/company/Hero.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Hero {
+    private int HeroHealth;
+    private int HeroDamage;
+    private String SuperHero;
+    public Hero (){}
+
+    public Hero(int heroDamage, int HeroHealth, String SuperHero) {
+        this.HeroDamage = heroDamage;
+        this.HeroHealth = HeroHealth;
+        this.SuperHero = SuperHero;
+    }
+
+    public Hero(int HeroHealth, int HeroDamage) {
+        this.HeroHealth = HeroHealth;
+        this.HeroDamage = HeroDamage;
+
+
+    }
+    public int getHeroHealth() {
+        return HeroHealth;
+    }
+
+    public int getHeroDamage() {
+        return HeroDamage;
+    }
+
+    public String getHeroDefenseType() {
+        return SuperHero;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Boss b1 = new Boss();
+        b1.setBossHealth(1000);
+        b1.setBossDamage(100);
+        b1.setBossDefenseType("Kinetic");
+        System.out.println(b1.getBossHealth() + " " + b1.getBossDamage() + " " + b1.getBossDefenseType());
+    }
+}
